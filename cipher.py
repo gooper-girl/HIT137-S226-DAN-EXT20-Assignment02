980ebda7de691a8970d28b265f998095cc6f53ec
gooper-girl <gabrielle.ld@pm.me>
2026-09-03 00:30:51 +0930

    Add initial cipher structure
    - Added prompts for shift 1 & 2 user input
    - Started defining the three functions encrypt_file, decrypt_file and verify_file

--- cipher.py
+++ cipher.py
@@ -1,3 +1,26 @@
-# reads entire file at once
-with open('raw.txt', 'r') as f:
-    raw_text = f.read()
+first_lower = "abcdefghijklmn"
+second_lower = "opqrstuvwxyz"
+first_upper = "ABCDEFGHIJKLM"
+second_upper = "NOPQRSTUVWXYZ"
+digits = "0123456789"
+
+shift1 = int(input("Enter shift1: "))
+shift2 = int(input("Enter shift2: "))
+
+
+def encrypt_file(shift1, shift2, input_path, output_path):
+    with open(input_path, 'r') as f:
+        raw_text = f.read()
+        
+    for char in raw_text:
+        if char.islower():
+            if char in first_lower:
+            if char in second_lower:
+        elif char.isupper():
+        elif char.isdigit():
+        else:
+            pass
+
+def decrypt_file(shift1, shift2, input_path, output_path):
+
+def verify_file(original_path, decrypted_path):
